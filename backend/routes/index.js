b0717576f1ad8a68be84f73ade0313bd7d0f9246
OussamaJoo <oussama.joo@itbs.tn>
2022-11-28 08:15:16 +0100

    Api pharmacie

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const authRoutes = require('./auth.route');
 const usersRoutes = require('./user.route');
+const pharmacieRoutes = require('./pharmacie.route');
 
 const router = express.Router();
 
 // localhost:4050/api/
 router.use('/auth', authRoutes);
 router.use('/users', usersRoutes);
+router.use('/pharmacie', pharmacieRoutes);
 
 module.exports = router;
